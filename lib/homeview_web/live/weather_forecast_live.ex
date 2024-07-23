04050ccca11ef83d0abfb8b3284a09eaf8b09d8e
Kent <kentaretorvik@gmail.com>
2024-07-23 19:18:55 +0200

    fix: add so that I use iframe for the card

--- lib/homeview_web/live/weather_forecast_live.ex
+++ lib/homeview_web/live/weather_forecast_live.ex
@@ -17,14 +17,13 @@ defmodule HomeviewWeb.WeatherForecastLive do
   def render(assigns) do
     ~H"""
     <div>
-    <.show_button show={@show} />
-    <div :if={@show == :local}
-    class="px-2 w-full h-full overflow-x-auto center-child-svg"
-    >
-      <iframe srcdoc={@local} class="min-h-[50vh] w-full"></iframe>
+      <.show_button show={@show} />
+      <div :if={@show == :local} class="px-2 w-full h-full overflow-x-auto center-child-svg">
+        <iframe src="https://www.yr.no/nn/innhald/1-2333502/card.html" class="min-h-[50vh] w-full">
+        </iframe>
       </div>
-    <div :if={@show == :long} class="px-2 w-full h-full overflow-x-auto center-child-svg">
-      <%= raw(@long) %>
+      <div :if={@show == :long} class="px-2 w-full h-full overflow-x-auto center-child-svg">
+        <%= raw(@long) %>
       </div>
     </div>
     """
@@ -35,16 +34,15 @@ defmodule HomeviewWeb.WeatherForecastLive do
 
   def show_button(assigns) do
     ~H"""
-
     <div class="flex justify-center">
-    <div class="flex gap-1 p-1 rounded-md w-64 bg-slate-200">
-    <.single_show_button active={@show == :local} on_click="show_local">
-    Show local
-    </.single_show_button>
-    <.single_show_button active={@show == :long} on_click="show_long">
-    Show long
-    </.single_show_button>
-    </div>
+      <div class="flex gap-1 p-1 rounded-md w-64 bg-slate-200">
+        <.single_show_button active={@show == :local} on_click="show_local">
+          Show local
+        </.single_show_button>
+        <.single_show_button active={@show == :long} on_click="show_long">
+          Show long
+        </.single_show_button>
+      </div>
     </div>
     """
   end
@@ -55,10 +53,13 @@ defmodule HomeviewWeb.WeatherForecastLive do
 
   def single_show_button(assigns) do
     ~H"""
-    <button class={"h-9 px-2 grow #{if assigns.active do
+    <button
+      class={"h-9 px-2 grow #{if assigns.active do
     "rounded-md bg-white text-sky-600"
-    end}"}  phx-click={assigns.on_click}>
-        <%= render_slot(@inner_block) %>
+    end}"}
+      phx-click={assigns.on_click}
+    >
+      <%= render_slot(@inner_block) %>
     </button>
     """
   end
